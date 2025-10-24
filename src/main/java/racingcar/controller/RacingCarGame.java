fb30597689637a05560aed731adda50674dab9cc
wmw04087 <wmw04087@gmail.com>
2025-10-25 02:31:27 +0900

    refactor: 컨트롤러 로직들을 메서드 단위로 분리

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -15,27 +15,47 @@ public class RacingCarGame {
     private final OutputView outputView = new OutputView();
 
     public void run() {
+        Names names = readInputCarNames();
+        Cars cars = Cars.from(names);
+        TryCount tryCount = readInputTryCount();
+
+        race(tryCount, cars);
+
+        showWinners(cars);
+    }
+
+    private Names readInputCarNames() {
         outputView.requestInputCarNames();
         String inputCarNames = inputView.read();
-        Names names = Names.from(inputCarNames);
 
+        return Names.from(inputCarNames);
+    }
+
+    private TryCount readInputTryCount() {
         outputView.requestInputTryCount();
         String inputTryCount = inputView.read();
-        TryCount tryCount = TryCount.from(inputTryCount);
 
-        Cars cars = Cars.from(names);
+        return TryCount.from(inputTryCount);
+    }
 
+    private void race(TryCount tryCount, Cars cars) {
         outputView.showResultMessage();
+
         while (tryCount.tryNext()) {
             cars.moveAll(Randoms::pickNumberInRange);
-
-            List<CarDto> carDtos = cars.getCars().stream()
-                    .map(CarDto::from)
-                    .toList();
-
-            outputView.showAllCarsResult(carDtos);
+            showAllCarsResult(cars);
         }
+    }
 
+    private void showAllCarsResult(Cars cars) {
+        List<CarDto> carDtos = cars.getCars().stream()
+                .map(CarDto::from)
+                .toList();
+
+        outputView.showAllCarsResult(carDtos);
+    }
+
+    private void showWinners(Cars cars) {
         List<Car> winners = cars.findWinners();
         List<String> winnerNames = winners.stream()
                 .map(Car::getName)
